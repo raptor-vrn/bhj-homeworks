fefbe93b644ac5b489fa85cc51c7b91b35037d37
Ivan <taran@nxt.ru>
2020-01-04 22:33:04 +0300

    edited-v2 2 task, 2 lessons

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,41 +1,21 @@
 'use strict';
 
-const menuLinks = document.querySelectorAll('.menu__link');
+document.addEventListener('click', (event) => {
 
-menuLinks.forEach(link => {
-    link.onclick = function (event) {
-        const subMenu = event.target.nextElementSibling;
+    const subMenu = event.target.nextElementSibling;
 
-        if (!subMenu) return;
+    if (!subMenu) return;
 
-        if(subMenu.classList.contains('menu_sub')) {
-            event.preventDefault();
-            subMenu.classList.toggle('menu_active')
+    if (subMenu.classList.contains('menu_sub')) {
+        event.preventDefault();
+        const subMenuActive = document.querySelector('.menu_active');
 
-            const siblings = getSiblings(subMenu.closest('.menu__item'))
-
-            siblings.forEach(element => {
-                const childrens = Array.from(element.children);
-                childrens.forEach(children => {
-                    if(children.classList.contains('menu_sub')) {
-                        children.classList.remove('menu_active')
-                    }
-                })
-            })
+        if (subMenuActive) {
+            subMenu.classList.add('menu_active')
+            subMenuActive.classList.remove('menu_active');
+            return
         }
-    };
-});
 
-const getSiblings = (elem) => {
-    const siblings = [];
-    let sibling = elem.parentNode.firstChild;
-
-    while (sibling) {
-        if (sibling.nodeType === 1 && sibling !== elem) {
-            siblings.push(sibling);
-        }
-        sibling = sibling.nextSibling
+        subMenu.classList.toggle('menu_active')
     }
-
-    return siblings;
-};
+});
