c18522a438d740618a8b861fd3e3c8a17ab791b0
Ivan <taran@nxt.ru>
2020-01-08 19:44:43 +0300

    did 2 task, 3 lessons

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,21 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    let inputtedSymbol;
+    let current = this;
+
+    function setSymbol() {
+      let symbol = current.currentSymbol.textContent.toUpperCase();
+      inputtedSymbol = String.fromCharCode(event.keyCode);
+
+      if (symbol == inputtedSymbol) {
+        current.success();
+      } else {
+        current.fail();
+      }
+    }
+
+    document.addEventListener('keydown', setSymbol);
   }
 
   success() {
