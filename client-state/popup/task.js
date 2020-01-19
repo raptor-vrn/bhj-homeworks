b16e1da53e08497aa644b19d774ef679a9f5e208
Ivan <taran@nxt.ru>
2020-01-19 13:49:35 +0300

    lessons 8, did 2 tasks

---
+++ client-state/popup/task.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function funonload() {
+    const modal = document.getElementById('subscribe-modal');
+    const closeBtns = document.querySelector('.modal__close');
+    modal.classList.add('modal_active');
+
+    closeBtns.addEventListener('click', function () {
+        modal.classList.remove('modal_active');
+    });
+
+    document.cookie = "modal=hidden";
+}
+
+const getCookie = (name) => {
+
+    const value = "; " + document.cookie;
+    let parts = value.split("; " + name + "=");
+
+    if (parts.length === 2) {
+
+        return parts
+            .pop()
+            .split(";")
+            .shift();
+    }
+};
+
+let result = getCookie('modal');
+
+if (!result) {
+    window.onload = funonload;
+}
+//document.cookie = 'modal=; Expires=Thu, 01 Jan 1970 00:00:00 GMT';
