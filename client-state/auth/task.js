8ba2fe551e92b4e42d4a7389b36b4ae32945fe25
Ivan <taran@nxt.ru>
2020-01-21 14:05:08 +0300

    lessons 8, did 3 task

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,12 +1,37 @@
 'use strict';
 
+const user = document.getElementById('welcome');
+const userID = document.getElementById('user_id');
+const signin = document.querySelector('.signin');
 const visForm = () => {
-    const signin = document.querySelector('.signin');
     signin.classList.add('signin_active');
-}
+};
 window.onload = visForm;
 
 const form = document.getElementById('signin__form');
-form.addEventListener('click',  (e) => {
+form.addEventListener('submit',  (e) => {
     let formData = new FormData(form);
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+    xhr.responseType = 'json';
+    xhr.send(formData);
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4) {
+            let request = xhr.response;
+
+            if (request['success'] != true) {
+                alert('Вы ввели неправильный логин/пароль')
+            } else {
+                localStorage.setItem('id_user', request['user_id']);
+                signin.classList.remove('signin_active');
+                user.classList.add('welcome_active');
+                userID.innerText = request['user_id'];
+
+            }
+        }
+    };
+    e.preventDefault();
 });
+
+let saveUserId = localStorage.getItem('id_user');
+console.log(saveUserId);
